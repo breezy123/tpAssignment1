24ac995c0efdd6fa30f4c5abf065d44dd161d19a
breezy123 <aufredericks@gmail.com>
2017-03-15 01:14:53 -0700

    java program initiated

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by aubrey on 15/03/2017.
+ */
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.print("Hey Welcome to Technical Programming II.");
+    }
+}
